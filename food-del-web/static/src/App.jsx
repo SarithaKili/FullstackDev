abfec6959350b8446bce2de837d04dcc4b9ff6f4
Saritha Kili <sarithakili7@gmail.com>
2024-11-03 18:58:04 +0530

    Your commit message here

--- food-del-web/static/src/App.jsx
+++ food-del-web/static/src/App.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Routes } from 'react-route-dom'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
+
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+    {showLogin?<LoginPopup/>:<></>}
     <div className='app'>
-      <Navbar/>
-      <Routes></Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
+    <Footer/>
+    </>
+    
   )
 }
 
-export default App
+export default App;
---
+++ food-del-web/static/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginPage.css'
+
+const LoginPage = () => {
+  return (
+    <div className='login-popup'>
+       login
+    </div>
+  )
+}
+
+export default LoginPage
