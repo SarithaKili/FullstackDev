e05226ccade238476b9943439bd2da8b44058ec8
Saritha Kili <sarithakili7@gmail.com>
2024-11-04 19:56:00 +0530

    login page signin psge

--- food-del-web/static/src/App.jsx
+++ food-del-web/static/src/App.jsx
@@ -5,15 +5,16 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Footer from './components/Footer/Footer'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './components/LoginPage/LoginPopup'
 
 const App = () => {
 
-  const [showLogin,setShowLogin]=useState(false)
+   const [showLogin,setShowLogin]=useState(false)
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar />
+      <Navbar setShowLogin={setShowLogin} />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Cart' element={<Cart />} />
