e60602fcc65a68f44bf427ce726dc47f362b9256
Saritha Kili <sarithakili7@gmail.com>
2024-11-05 20:58:26 +0530

    updated cart page

--- food-del-web/static/src/components/Navbar/Navbar.jsx
+++ food-del-web/static/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu] = useState("home")
   return (
     <div className='navbar'>
     
-   <img src={assets.logo} alt='' className='logo'/>
+   <Link to='/'><img src={assets.logo} alt='' className='logo'/></Link>
    <ul className="navbar-menu">
     <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
     <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
@@ -18,7 +19,7 @@ const Navbar = ({setShowLogin}) => {
    <div className="navbar-right">
     <img src={assets.search_icon} alt=''/>
     <div className="navbar-search-icon">
-      <img src={assets.basket_icon} alt=''/>
+      <Link to='/cart' ><img src={assets.basket_icon} alt=''/> </Link>
       <div className="dot"></div>
     </div>
     <button onClick={()=>setShowLogin(true)} >sign in</button>
--- food-del-web/static/src/pages/Cart/Cart.jsx
+++ food-del-web/static/src/pages/Cart/Cart.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import './Cart.css'
-
+import React, { useContext } from 'react';
+import './Cart.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className='cart'>
+      <div className='cart-items'>
+        <div className='cart-items-title'>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index} className='cart-items-item cart-items-title'>
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price * cartItems[item._id]}</p>
+                <p>X</p>
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
